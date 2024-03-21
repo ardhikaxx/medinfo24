125b74c848df4d3b6d632637a9108bbc7c35da4d
ardhikaxx <ardhikayanuar58@gmail.com>
2024-03-21 07:43:32 +0700

    update-add

--- assets/js/hubungi.js
+++ assets/js/hubungi.js
@@ -2,44 +2,79 @@ const btnHubungi = document.querySelector('.btn-hubungi');
 
 btnHubungi.addEventListener('click', function () {
     Swal.fire({
-        title: 'Konfirmasi Persyaratan',
-        text: 'Apakah Anda sudah melakukan screenshoot minimal 10 akun aktif untuk follow Instagram BEM KM POLIJE @bempolije dan subscribe Youtube BEM POLIJE CHANNEL serta menyertakan bukti follow?',
+        title: 'Konfirmasi Internal/Eksternal',
+        text: 'Apakah Anda adalah bagian dari internal atau eksternal?',
         icon: 'question',
         iconColor: '#4F6F52',
         showCancelButton: true,
-        confirmButtonText: 'Ya',
-        cancelButtonText: 'Belum',
+        confirmButtonText: 'Internal',
+        cancelButtonText: 'Eksternal',
         cancelButtonColor: '#4F6F52',
         confirmButtonColor: '#3A4D39',
     }).then((result) => {
         if (result.isConfirmed) {
-            Swal.fire({
-                title: 'Konfirmasi Pembayaran',
-                text: 'Apakah Anda sudah mentransfer sebesar Rp 25.000,- melalui rekening (a.n Zulfianti Rahmawati : Dana – 085747493573)?',
-                icon: 'question',
-                iconColor: '#4F6F52',
-                showCancelButton: true,
-                confirmButtonText: 'Ya',
-                cancelButtonText: 'Belum',
-                cancelButtonColor: '#4F6F52',
-                confirmButtonColor: '#3A4D39',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire({
-                        title: 'Pemberitahuan',
-                        text: 'Setelah Anda selesai melakukan pengisian data, Anda dapat mengirim bukti screenshoot 10 akun dan bukti transfer!',
-                        icon: 'info',
-                        iconColor: '#4F6F52',
-                        showConfirmButton: true,
-                        confirmButtonColor: '#3A4D39',
-                        confirmButtonText: 'Lanjut'
-                    }).then(() => {
-                        tampilkanFormInputData();
-                    });
-                }
-            });
+            tampilkanFormInputData();
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+                Swal.fire({
+                    title: 'Konfirmasi Persyaratan',
+                    text: 'Apakah Anda sudah melakukan screenshoot minimal 10 akun aktif untuk follow Instagram BEM KM POLIJE @bempolije dan subscribe Youtube BEM POLIJE CHANNEL serta menyertakan bukti follow?',
+                    icon: 'question',
+                    iconColor: '#4F6F52',
+                    showCancelButton: true,
+                    confirmButtonText: 'Ya',
+                    cancelButtonText: 'Belum',
+                    cancelButtonColor: '#4F6F52',
+                    confirmButtonColor: '#3A4D39',
+                }).then((resultSyarat) => {
+                    if (resultSyarat.isConfirmed) {
+                        Swal.fire({
+                            title: 'Konfirmasi Pembayaran',
+                            text: 'Apakah Anda sudah mentransfer sebesar Rp 25.000,- melalui rekening (a.n Zulfianti Rahmawati : Dana – 085747493573)?',
+                            icon: 'question',
+                            iconColor: '#4F6F52',
+                            showCancelButton: true,
+                            confirmButtonText: 'Ya',
+                            cancelButtonText: 'Belum',
+                            cancelButtonColor: '#4F6F52',
+                            confirmButtonColor: '#3A4D39',
+                        }).then((resultPembayaran) => {
+                            if (resultPembayaran.isConfirmed) {
+                                Swal.fire({
+                                    title: 'Pemberitahuan',
+                                    text: 'Setelah Anda selesai melakukan pengisian data anda, Anda dapat mengirim bukti screenshoot minimal 10 akun dan bukti transfer!',
+                                    icon: 'info',
+                                    iconColor: '#4F6F52',
+                                    showConfirmButton: true,
+                                    confirmButtonColor: '#3A4D39',
+                                    confirmButtonText: 'Lanjut'
+                                }).then(() => {
+                                    tampilkanFormInputData();
+                                });
+                            } else if (resultPembayaran.dismiss === Swal.DismissReason.cancel) {
+                                Swal.fire({
+                                    title: 'Anda Belum Melakukan Pembayaran',
+                                    text: 'Silakan melakukan pembayaran terlebih dahulu.',
+                                    icon: 'info',
+                                    iconColor: '#4F6F52',
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                });
+                            }
+                        });
+                    } else if (resultSyarat.dismiss === Swal.DismissReason.cancel) {
+                        Swal.fire({
+                            title: 'Anda Belum Memenuhi Syarat',
+                            text: 'Anda bisa memenuhi syarat dan ketentuan terlebih dahulu.',
+                            icon: 'info',
+                            iconColor: '#4F6F52',
+                            timer: 2000,
+                            showConfirmButton: false
+                        });
+                    }
+                });
+            }
         }
-    });
+    )
 });
 
 function tampilkanFormInputData() {
@@ -60,7 +95,7 @@ function tampilkanFormInputData() {
 
             if (nama && organisasi && keperluan) {
                 const linkWhatsApp = 'https://wa.me/6285747493573';
-                const pesan = `Assalamualaikum Wr. Wb. kak,\n\nPerkenalkan, saya ${nama} dari ${organisasi}. Kami tertarik untuk menjadi media partner dalam tujuan ${keperluan} dan ingin mendapatkan persetujuan dari BEM KM POLIJE. Kami berharap dapat bekerja sama dengan baik untuk kepentingan bersama.\n\nTerima kasih atas perhatiannya, dan kami menunggu tanggapan dari BEM KM POLIJE.\n\nSalam.`;
+                const pesan = `Assalamualaikum Wr. Wb. kak,\n\nPerkenalkan, nama saya ${nama} dari ${organisasi}. Kami tertarik untuk menjadi media partner dalam tujuan ${keperluan} dan ingin mendapatkan persetujuan dari BEM KM POLIJE. Kami berharap dapat bekerja sama dengan baik untuk kepentingan bersama.\n\nTerima kasih atas perhatiannya, dan kami menunggu tanggapan dari BEM KM POLIJE.\n\nSalam.`;
 
                 window.open(`${linkWhatsApp}?text=${encodeURIComponent(pesan)}`);
             } else {
